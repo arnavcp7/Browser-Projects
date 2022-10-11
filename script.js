3764a216b143172324697e946d48a2634630197d
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-10-12 00:26:26 +0530

    Weather API app
    GitHub is a web-based hosting service for open source projects and the go-to place for the open source community to host and collaborate on their projects.
    The service is free and allows you to host private and public repositories for free.
    Once you sign up and login, you will be able to create a new repository or fork another one.
    Then, you will start sharing your code with the world.
    The community will start using it, improving it, and changing it

--- script.js
+++ script.js
@@ -1,30 +1,44 @@
-let inputTag = document.querySelector("input");
-let ulTag = document.querySelector("ul");
+/**
+ * Weather App
+ */
 
-inputTag.addEventListener('keydown',function(e){
-    // console.log(e);
-    let key = e.key;
-    if(key == 'Enter'){
-        let value = inputTag.value;
-        // console.log(value);
-        if(value.length == 0){
-            return;
-        }
-        inputTag.value = ""; //ui input update 
-        let liTag = document.createElement("li");
-        liTag.innerHTML = `<div>${value}</div>
-                            <div class="delete"><i class="fa fa-trash"></i></div>`
+// API_KEY for maps api
+let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
 
-        //other way
-        // liTag.innerHTML = "<div>"+value+"</div><div><i class='fa fa-trash'></i></div>"
-        handleRemoval(liTag);
-        ulTag.appendChild(liTag);
-    }
-})
-
-function handleRemoval(liTag){
-    let deleteDiv = liTag.querySelector(".delete"); 
-    deleteDiv.addEventListener("click",function(){
-        liTag.remove();
-    })
+/**
+ * Retrieve weather data from openweathermap
+ */
+const getWeatherData = (city) => {
+  const URL = "https://api.openweathermap.org/data/2.5/weather";
+  const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
+  const weatherPromise  = fetch(FULL_URL);
+  return weatherPromise.then((response) => {
+    return response.json();
+  })
 }
+
+/**
+ * Retrieve city input and get the weather data
+ */
+const searchCity = () => {
+  const city = document.getElementById('city-input').value;
+  getWeatherData(city)
+  .then((res)=>{
+    showWeatherData(res);
+  }).catch((error)=>{
+    console.log(error);
+    console.log("Something happened");
+  })
+}
+
+/**
+ * Show the weather data in HTML
+ */
+showWeatherData = (weatherData) => {
+  document.getElementById("city-name").innerText = weatherData.name;
+  document.getElementById("weather-type").innerText = weatherData.weather[0].main;
+  document.getElementById("temp").innerText = weatherData.main.temp;
+  document.getElementById("min-temp").innerText = weatherData.main.temp_min;
+  document.getElementById("max-temp").innerText = weatherData.main.temp_max;
+}
+
